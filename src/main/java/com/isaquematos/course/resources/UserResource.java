4f326ab20a6026dd7e27c25fb3dd492adae4a0ab
Isaque Matos <isaque.smatos@hotmail.com>
2025-11-17 15:30:07 -0300

    Service layer, component registration

--- src/main/java/com/isaquematos/course/resources/UserResource.java
+++ src/main/java/com/isaquematos/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.isaquematos.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isaquematos.course.entities.User;
+import com.isaquematos.course.services.UserService;
 
 @RestController //Cria controladores, indica que a classe contém métodos que vão responder com dados diretamente (JSON ou XML)
 @RequestMapping(value = "/users") // anotação usada para mapear requisições HTTP para métodos específicos
 public class UserResource {
-
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping //indica que o método vai tratar requisições GET, ou seja, obter dados de um servidor
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")//indica q a requisição aceita um id dentro da url
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
