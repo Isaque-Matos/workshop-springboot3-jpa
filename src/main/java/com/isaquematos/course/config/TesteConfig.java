910306a80c3677c4d1d7a0cb7e660eaac0767d89
Isaque Matos <isaque.smatos@hotmail.com>
2025-11-21 21:07:00 -0300

    Payment, one-to-one association

--- src/main/java/com/isaquematos/course/config/TesteConfig.java
+++ src/main/java/com/isaquematos/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.isaquematos.course.entities.Category;
 import com.isaquematos.course.entities.Order;
 import com.isaquematos.course.entities.OrderItem;
+import com.isaquematos.course.entities.Payment;
 import com.isaquematos.course.entities.Product;
 import com.isaquematos.course.entities.User;
 import com.isaquematos.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
