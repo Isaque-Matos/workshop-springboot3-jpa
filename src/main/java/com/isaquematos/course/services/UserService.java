758178801c3fa99ac77d11c913176107e09ed938
Isaque Matos <isaque.smatos@hotmail.com>
2025-11-22 16:56:48 -0300

    Exception handling - update

--- src/main/java/com/isaquematos/course/services/UserService.java
+++ src/main/java/com/isaquematos/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.isaquematos.course.repositories.UserRepository;
 import com.isaquematos.course.services.exceptions.DatabaseException;
 import com.isaquematos.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service //package de serviços, cuida das regras de negócio/lógica
 public class UserService {
 	
@@ -48,9 +50,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
